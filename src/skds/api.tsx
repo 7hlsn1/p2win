a6a7347b142852eacb79af3cab988ac313dae433
root <root@heaven.heaven.local>
2025-04-15 15:50:25 -0300

    SDK Updates

--- src/skds/api.tsx
+++ src/skds/api.tsx
@@ -1,8 +1,19 @@
 import axios from "axios";
+
 interface Category {
     id: number,
     name: string,
     image: string
+}
+
+interface Product {
+    id: number,
+    title: string,
+    banner: string,
+    price: number,
+    category_id: number,
+    user_id: number,
+    created_at: string
 }
 
 interface Auth {
@@ -26,19 +37,27 @@ class Api {
     }
     static api = axios.create(Api.data)
 
- 
+
 
     login = (email: string, password: string) => new Promise(async (resolve, reject) => {
         const req = await Api.api.post('/login', [{ email, password }]);
         resolve(req.data)
         if (!req) {
-            reject('err')
+            reject()
         }
     })
 
+
+    register = (email: string, password: string, username: string) => new Promise(async (resolve, reject) => {
+        const req = await Api.api.post('/register', [{ email, password, username }]);
+        resolve(req.data)
+        if (!req) {
+            reject()
+        }
+    })
+
+
     getCategories = (search: string = '') => new Promise(async function (resolve, reject) {
-
-
         const req = await Api.api.get('/categories?search=' + search)
         const data = req.data
         var result = [];
@@ -46,20 +65,38 @@ class Api {
             let data_: Category = data[i];
             result.push(data_);
         }
-        resolve(result); // when successful
-        reject();  // when error
+        resolve(result);
+        reject();
     });
 
 
+    getProducts = (search: string = '', user: any = '') => new Promise(async function (resolve, reject) {
+        const req = await Api.api.get(`/products?search=${search}&user=${user}`)
+        const data = req.data
+        var result = [];
+        for (var i in data) {
+            let data_: Product = data[i];
+            result.push(data_);
+        }
+        resolve(result);
+        reject();
+    });
 
+    createProduct = (category: number, type: number, title: string, description: string, price: number, banner: string) => new Promise(async function (resolve, reject) {
+        const req = await Api.api.post(`/products/create`, [{ category, type, title, description, price, banner }])
+        resolve(req.data);
+        reject();
+    });
 
-    static getProducts = async () => {
-
-    }
+    likeProduct = (id: number) => new Promise(async (resolve, reject) => {
+        const req = await Api.api.get(`/products/like/${id}`)
+        resolve(req.data)
+        reject()
+    })
 
 }
 
 
 
 export { Api }
-export type { Category, Auth }
+export type { Category, Auth, Product }
