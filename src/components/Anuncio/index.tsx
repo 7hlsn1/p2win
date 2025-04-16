40281e79bfb5bbb92ff37c5bd95002f3529318b6
root <root@heaven.heaven.local>
2025-04-15 21:00:50 -0300

    Update

--- src/components/Anuncio/index.tsx
+++ src/components/Anuncio/index.tsx
@@ -1,11 +1,17 @@
 import styles from './Anuncio.module.scss'
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import { Api } from '../../skds/api';
+const api = new Api('open')
 const Anuncio: React.FC = () => {
   const [banner, setBanner] = useState('');
-
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    api.getCategories().then((data: any) => {
+      setCategories(data)
+    })
+  }, [])
   const handleSetBanner = (e: any) => {
-     
+
     setBanner(URL.createObjectURL(e.target.files[0]))
   }
   return (
@@ -35,6 +41,20 @@ const Anuncio: React.FC = () => {
       </div>
       <div className={styles.inlineInputs}>
         <div className={styles.formGroup}>
+          <label htmlFor="category_id">Escolha uma categoria para o seu anúncio</label>
+          <select name="category_id" id="category_id">
+            {
+              categories.map((category: any) => {
+                return (
+                  <option key={category.id}>{category.name}</option>
+                )
+              })
+            }
+          </select>
+        </div>
+      </div>
+      <div className={styles.inlineInputs}>
+        <div className={styles.formGroup}>
           <label htmlFor="valor">Valor do anúncio</label>
           <input type="number" step="0.1" id="valor" placeholder="R$ 0,00" />
         </div>
@@ -49,7 +69,7 @@ const Anuncio: React.FC = () => {
       {banner != '' ? (
         <div className={styles.inlineInputs}>
           <div className={styles.formGroup}>
-            <img src={banner} alt="" style={{width: '100%'}}/>
+            <img src={banner} alt="" style={{ width: '100%' }} />
           </div>
         </div>
       ) : <>
