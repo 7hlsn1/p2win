cd73bdce062e18fdf2a0cdea8fe806567e26601e
root <root@heaven.heaven.local>
2025-05-15 21:04:19 -0300

    Orderr updates

---
+++ src/pages/MailError/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MailError: React.FC = () => {
+  return (
+    <div>
+      <h3>
+        Houve um erro ao verificar sua conta, por favor contate o suporte
+      </h3>
+    </div>
+  )
+};
+
+export default MailError;
