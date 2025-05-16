9fa76d626a6478e3f472b54bf836b48e263debcd
root <root@heaven.heaven.local>
2025-05-15 21:01:19 -0300

    Orderr updates

--- src/pages/MailSuccess/index.tsx
+++ src/pages/MailSuccess/index.tsx
@@ -5,7 +5,7 @@ const MailSuccess: React.FC = () => {
   return (
     <div>
       <h3>
-        Seu email foi verificado com sucesso, você já pode fazer o <Link to={'/login'}>login</Link> na sua conta
+        Seu email foi verificado com sucesso, você já pode fazer o <Link  className='link' to={'/login'}>login</Link> na sua conta
       </h3>
     </div>
   )
